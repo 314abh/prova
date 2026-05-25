5f3f3250d2053699e15fe79d22e7618512eb13d0
abh <314abh@gmail.com>
2026-05-25 07:24:23 +0530

    added: refactoring

---
+++ src/next/prova.h
@@ -0,0 +1,13 @@
+#ifndef PROVA_H
+#define PROVA_H
+
+#include "prova_defs.h"
+
+#include <threads.h>
+
+extern ProvaTest *PROVA_TEST_QUEUE;
+extern _Thread_local ProvaTest *PROVA_CURRENT_TEST;
+
+
+
+#endif /* PROVA_H */
